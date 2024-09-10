f37b15a5204c7cef8b380c21175d04e19f9be966
purvaja1105 <dahanepurvaja0@gmail.com>
2024-09-10 14:13:49 +0530

    Add files via upload

---
+++ BlogApplication1/BlogApplication/src/main/java/com/servlet/AddServlet.java
@@ -0,0 +1,46 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.connection.FactoryProvider;
+import com.model.Blog;
+
+
+public class AddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String title = request.getParameter("title");
+		String author = request.getParameter("author");
+		String desc = request.getParameter("description");
+		Blog b1 = new Blog();
+		Session session = FactoryProvider.getFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		b1.setTitle(title);
+		b1.setAuthor(author);
+		b1.setDescription(desc);
+
+		session.save(b1);
+
+		tx.commit();
+		session.close();
+		//out.println("<h1 style='text-align=center;'>Blog added Successfully</h1>");
+		response.sendRedirect("show.jsp");
+	}
+
+}
---
+++ BlogApplication1/BlogApplication/src/main/webapp/about.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%@ include file="all_css.html" %>
+<br>
+<h1 style='text-align:center'><b><I>This is About Page</I></b></h1>
+
+</body>
+</html>
